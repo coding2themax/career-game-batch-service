8c79c04c60b4f828add8a239548c049fb7748d3b
max quinones <maximilian.quinones@gmail.com>
2023-12-07 21:17:12 -0700

    init

---
+++ src/main/java/com/coding2themax/career/game/careergamebatchservice/CareerGameBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.game.careergamebatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerGameBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerGameBatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding2themax/career/game/careergamebatchservice/CareerGameBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.game.careergamebatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGameBatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
